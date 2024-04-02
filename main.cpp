7ae93a8ab7abd31991d475560a249cfac1c0d927
shadykun <soltuzraresflorin2004@gmail.com>
2024-04-02 11:41:01 +0300

    Start of the menu

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+#include "game.h"
+#include "character.h"
+
+int main()
+{
+    int choice;
+    std::cout << "\n\nWELCOME TO POOLAND\n\nChose an option: \n";
+    std::cout << "1. Play\n2. Quit (please don't)\n\nChoice:";
+    std::cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        Game;
+        break;
+    case 2:
+        std::cout<<"\n\nFor real? Fine, go and play outside!\n\n";;
+        return;
+    default:
+        std::cout<<"\n\nNot an option, try again!";
+        break;
+    }
+}
